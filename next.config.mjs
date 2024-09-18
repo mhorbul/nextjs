c219cf15167602efe84da054c6b78d554e020d3a
Maksim Horbul <maksim.horbul@smarterdx.com>
2024-09-17 22:30:34 -0400

    introduce nextjs var

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    APP_NAME_VAR: process.env.APP_NAME,
+  },
 };
 
 export default nextConfig;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,13 +2,19 @@ class Foo {
     constructor() {
     }
 
-    static configure(appName) {
+    static configure(appName, appNameNextJsVar) {
         Foo.appName = appName;
+        Foo.appNameNextJsVar = appNameNextJsVar;
     }
 }
 
-Foo.configure(process.env.APP_NAME);
+Foo.configure(process.env.APP_NAME, process.env.APP_NAME_VAR);
 
 export default function handler(req, res) {
-    res.status(200).json({ "Foo.appName": Foo.appName, "process.env.APP_NAME": process.env.APP_NAME })
+    res.status(200).json({
+        "Foo.appName": Foo.appName,
+        "Foo.appNameNextJsVar": Foo.appNameNextJsVar,
+        "process.env.APP_NAME": process.env.APP_NAME,
+        "process.env.APP_NAME_VAR": process.env.APP_NAME_VAR,
+    })
 }
